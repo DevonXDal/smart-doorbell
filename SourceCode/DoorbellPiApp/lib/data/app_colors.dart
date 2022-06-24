5f8d989eb5bfd6cfbfe121d6a25bfc2c49e54cfa
Devon Dalrymple <devonxpersonal@gmail.com>
2022-06-24 14:31:46 -0400

    Added theme colors to Flutter app

---
+++ SourceCode/DoorbellPiApp/lib/data/app_colors.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+/// This AppColors class is used to provide static names for commonly used theme colors of the app.
+///
+/// Author: Devon X. Dalrymple
+/// Version 2022-06-24
+abstract class AppColors {
+  static const Color pageBackgroundPurple = Color.fromARGB(255, 94, 73, 85);
+
+  static const Color navigationBackgroundPink = Color.fromARGB(255, 149, 100, 132);
+
+  static const Color pageComponentBackgroundDeepDarkBlue = Color.fromARGB(255, 0, 39, 43);
+
+  static const Color textForegroundOrange = Color.fromARGB(255, 242, 255, 73);
+
+  static const Color transparentOptionsBlue = Color.fromARGB(127, 0, 51, 56);
+}
